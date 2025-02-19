2435e86cd820d313a7e57df2f4ca06db59f851e0
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2025-02-19 20:00:24 +0700

    intial project

---
+++ all-stu-product/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <button class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+      Hello Tailwind!
+    </button>
+  </div>
+</template>
